451ccb639566e2bb04ca57663fd33115fd34e2bb
ElioLopez <35702545+ElioLopez@users.noreply.github.com>
2021-02-28 16:35:12 +0100

    blog commit

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,14 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
-};
+import jsonPlacecholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+        const response = await jsonPlacecholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response.data })
+    };  
+
+
+ export const fetchUser = (id) => async (dispatch) => {
+     const response = await jsonPlacecholder.get(`/users/{$id}`);
+
+     dispatch({ type: 'FETCH_USER', payload: response.data })
+ };  
