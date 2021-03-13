a2d93f479f1d10bb8fa2ed613dbb569c172eb4fc
Elio Rene Lopez <elio.lopez@gmail.com>
2021-03-13 12:40:26 +0100

    streams

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -3,69 +3,63 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-  componentDidMount() {
-    window.gapi.load('client:auth2', () => {
-      window.gapi.client
-        .init({
-          clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
-          scope: 'email'
-        })
-        .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-
-          this.onAuthChange(this.auth.isSignedIn.get());
-          this.auth.isSignedIn.listen(this.onAuthChange);
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '547660664448-b1iplrj8813vilt5nvgdhen0vjpaag9g.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange(this.auth.isSignedIn.get)
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
         });
-    });
-  }
-
-  onAuthChange = isSignedIn => {
-    if (isSignedIn) {
-      this.props.signIn(this.auth.currentUser.get().getId());
-    } else {
-      this.props.signOut();
     }
-  };
 
-  onSignInClick = () => {
-    this.auth.signIn();
-  };
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
+    };
 
-  onSignOutClick = () => {
-    this.auth.signOut();
-  };
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
 
-  renderAuthButton() {
-    if (this.props.isSignedIn === null) {
-      return null;
-    } else if (this.props.isSignedIn) {
-      return (
-        <button onClick={this.onSignOutClick} className="ui red google button">
-          <i className="google icon" />
-          Sign Out
-        </button>
-      );
-    } else {
-      return (
-        <button onClick={this.onSignInClick} className="ui red google button">
-          <i className="google icon" />
-          Sign In with Google
-        </button>
-      );
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
+
+
+    renderAuthButton(){
+        if (this.state.isSignedIn == null) {
+            return null;
+        } else if  (this.state.isSignedIn) {
+            return (
+            <button onClick={this.onSignOutClick} className="ui red google button">
+                <i className="google icon" />
+                Sign Out
+            </button>
+            );
+        } else {
+            return (
+                <button onClick={this.onSignInClick} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+                );
+            }
     }
-  }
 
-  render() {
-    return <div>{this.renderAuthButton()}</div>;
-  }
+    render() {
+        return <div>{this.renderAuthButton()}</div>;
+    }
 }
 
-const mapStateToProps = state => {
-  return { isSignedIn: state.auth.isSignedIn };
-};
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+}
 
-export default connect(
-  mapStateToProps,
-  { signIn, signOut }
-)(GoogleAuth);
+export default connect(null, {signIn, signOut })(GoogleAuth);
--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,6 +1,8 @@
 import { ECONNRESET } from 'constants';
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -25,8 +27,8 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues){
-        console.log(formValues);
+  onSubmit = (formValues) => {
+       this.props.createStream(formValues);
   }
 
   render() {
@@ -55,7 +57,9 @@ const validate = (formValues) => {
         return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
